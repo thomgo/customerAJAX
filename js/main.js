cf84835df9a783b391d06ab2c23ed1085f09c6f1
Thomas Gossart <tg.59290@gmail.com>
2017-07-01 13:32:45 +0200

    made base function to sort dynamicaly the json result

--- js/main.js
+++ js/main.js
@@ -1,21 +1,49 @@
+// Object to deal with xml requests
+var xhttp = new XMLHttpRequest();
+
+// Get the body table to add the lines
 var tBody = document.getElementById("tBody");
 
-var xhttp = new XMLHttpRequest();
+// Store the different customers
+var customers = null;
+
+// Function to creat a table line for each client from the request. Take a json mutlidimensional object as argument
+function createLines(jsonObject) {
+  for(var prop in jsonObject) {
+    var tableRow = document.createElement("TR");
+    for(var data in jsonObject[prop]) {
+      var tableCell = document.createElement("TD");
+      var textnode = document.createTextNode(jsonObject[prop][data]);
+      tableCell.appendChild(textnode);
+      tableRow.appendChild(tableCell);
+    }
+    tBody.appendChild(tableRow);
+  }
+}
+
+function clearTable(selectedNode) {
+  while (selectedNode.firstChild) {
+    selectedNode.removeChild(selectedNode.firstChild);
+}
+}
+
+var test = document.getElementById("test");
+
+
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
 
-      var customers = JSON.parse(this.responseText);
+      customers = JSON.parse(this.responseText);
+      createLines(customers);
 
-      for(var prop in customers) {
-        var tableRow = document.createElement("TR");
-        for(var data in customers[prop]) {
-          var tableCell = document.createElement("TD");
-          var textnode = document.createTextNode(customers[prop][data]);
-          tableCell.appendChild(textnode);
-          tableRow.appendChild(tableCell);
-        }
-        tBody.appendChild(tableRow);
-      }
+      test.onclick = function() {
+        clearTable(tBody);
+        customers.sort(function(a, b) {
+        return a.age - b.age;
+        });
+        createLines(customers);
+      };
+
    }
 };
 xhttp.open("GET", "js/customers.json", true);
